3d728abf0ad41fa193c2023e3198b31a505516b1
Mitica <canteadumitru@gmail.com>
2018-11-30 21:25:38 +0200

    select priority entity

---
+++ src/usecases/actions/select-wiki-entity.test.ts
@@ -0,0 +1,27 @@
+
+import test from 'ava';
+import { isPriorityEntity, sortWikiEntities } from './select-wiki-entity';
+import { FindWikiEntitiesByTitles } from './find-wiki-entities-by-titles';
+import { KnownNameService } from '../../services/known-names-service';
+
+
+
+test('isPriorityEntity', async t => {
+    const locale = { lang: 'ro', country: 'ro' };
+    const finder = new FindWikiEntitiesByTitles(locale, new LocaleKnownNames());
+    let entities = await finder.execute(['Siria']);
+
+    // t.true(entities.length > 1, 'more then 1 entities');
+    entities = sortWikiEntities(entities, 'ro');
+    const siria = entities[0];
+
+    t.is(siria.wikiDataId, 'Q858', 'first entity is Siria');
+    t.true(isPriorityEntity(siria), 'Siria is a priority entity');
+    // t.false(isPriorityEntity(entities[1]), 'Second entity is not a priority one');
+})
+
+class LocaleKnownNames implements KnownNameService {
+    getKnownName(_name: string, _lang: string, _country: string) {
+        return null;
+    }
+}
--- src/usecases/actions/select-wiki-entity.ts
+++ src/usecases/actions/select-wiki-entity.ts
@@ -28,14 +28,14 @@ export class SelectWikiEntity extends UseCase<string[], WikiEntity | null, void>
             debug(`NOT Found wikientity by names: ${JSON.stringify(names)}`);
         }
 
-        if (entities.length === 0 || this.countryWikiEntities(entities).length === 0) {
+        if (entities.length === 0 || countryWikiEntities(entities, this.locale.country).length === 0) {
             let entitiesByPartialNames: WikiEntity[] = []
             for (let nameHash of nameHashes) {
                 const list = await this.wikiEntityRepository.getByPartialNameHash(nameHash);
                 entitiesByPartialNames = entitiesByPartialNames.concat(list)
             }
 
-            entitiesByPartialNames = this.countryWikiEntities(entitiesByPartialNames);
+            entitiesByPartialNames = countryWikiEntities(entitiesByPartialNames, this.locale.country);
             if (entitiesByPartialNames.length) {
                 debug(`found locale entities by partial name: ${entitiesByPartialNames.map(item => item.name)}`);
                 entities = entities.concat(entitiesByPartialNames);
@@ -49,66 +49,61 @@ export class SelectWikiEntity extends UseCase<string[], WikiEntity | null, void>
 
         entities = uniqByProperty(entities, 'id');
 
-        entities = this.sortWikiEntities(entities);
+        entities = sortWikiEntities(entities, this.locale.country);
 
         const entity = entities[0];
 
         return entity;
     }
-
-    private sortWikiEntities(entities: WikiEntity[]): WikiEntity[] {
-        if (!entities.length) {
-            return entities;
-        }
-
-        entities = sortEntities(entities);
-
-        const topEntity = entities[0];
-
-        if (topEntity.countryCodes && topEntity.countryCodes.includes(this.locale.country)) {
-            debug(`Top entity has country=${this.locale.country}: ${topEntity.name}`);
-            return uniqByProperty(entities, 'id');
-        }
-
-
-        const countryEntities = this.countryWikiEntities(entities);
-        if (countryEntities.length) {
-            let useCountryEntity = false;
-            const topEntityPopularity = WikiEntityHelper.getPopularity(topEntity.rank);
-            const countryEntityPopularity = WikiEntityHelper.getPopularity(countryEntities[0].rank);
-
-            if (topEntityPopularity < EntityPopularity.POPULAR || countryEntityPopularity > EntityPopularity.LOW) {
-                useCountryEntity = true;
-            }
-            else {
-                debug(`using POPULAR entity: ${topEntity.name}(${topEntity.rank}-${topEntityPopularity})>${countryEntities[0].rank}-(${countryEntityPopularity})`);
-            }
-
-            if (useCountryEntity) {
-                entities = countryEntities.concat(entities);
-                debug(`using country entity: ${entities[0].name}`);
-            }
-        }
-
-        entities = uniqByProperty(entities, 'id');
-
-        return entities;
-    }
-
-    private countryWikiEntities(entities: WikiEntity[]): WikiEntity[] {
-        if (!entities.length) {
-            return entities;
-        }
-        return entities.filter(item => item.countryCodes && item.countryCodes.includes(this.locale.country));
-    }
 }
 
-function sortEntities(entities: WikiEntity[]) {
+export function sortWikiEntities(entities: WikiEntity[], country: string): WikiEntity[] {
     if (!entities.length) {
         return entities;
     }
 
     entities = entities.sort((a, b) => b.rank - a.rank);
 
+    const topEntity = entities[0];
+
+    if (topEntity.countryCodes && topEntity.countryCodes.includes(country)) {
+        debug(`Top entity has country=${country}: ${topEntity.name}`);
+        return entities;
+    }
+
+
+    const countryEntities = countryWikiEntities(entities, country);
+    if (countryEntities.length) {
+        let useCountryEntity = false;
+        const topEntityPopularity = WikiEntityHelper.getPopularity(topEntity.rank);
+        const countryEntityPopularity = WikiEntityHelper.getPopularity(countryEntities[0].rank);
+
+        useCountryEntity = topEntityPopularity < EntityPopularity.POPULAR || countryEntityPopularity > EntityPopularity.LOW;
+
+        if (useCountryEntity && isPriorityEntity(topEntity)) {
+            useCountryEntity = false;
+            debug(`using PRIORITY entity: ${topEntity.name}`)
+        } else {
+            debug(`using POPULAR entity: ${topEntity.name}(${topEntity.rank}-${topEntityPopularity})>${countryEntities[0].rank}-(${countryEntityPopularity})`);
+        }
+
+        if (useCountryEntity) {
+            entities = countryEntities.concat(entities);
+            debug(`using country entity: ${entities[0].name}`);
+        }
+        entities = uniqByProperty(entities, 'id');
+    }
+
     return entities;
+}
+
+export function isPriorityEntity(entity: WikiEntity) {
+    return entity.data && (entity.data['P31'] || []).includes('Q6256');
+}
+
+function countryWikiEntities(entities: WikiEntity[], country: string): WikiEntity[] {
+    if (!entities.length) {
+        return entities;
+    }
+    return entities.filter(item => item.countryCodes && item.countryCodes.includes(country));
 }
